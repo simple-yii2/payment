5d438087029ad70cef198312801ed8fb8207ed85
Dmitry Khlystov <dkhlystov@gmail.com>
2017-04-16 12:07:28 +1000

    register frontend module

--- common/components/Payment.php
+++ common/components/Payment.php
@@ -2,17 +2,39 @@
 
 namespace cms\payment\common\components;
 
+use yii\base\BootstrapInterface;
 use yii\base\Component;
 
 /**
  * Payment application component contains common information about payment providers and register payment module on bootstrap
  */
-class Payment extends Component
+class Payment extends Component implements BootstrapInterface
 {
 
 	/**
 	 * @var string[]|array[]|ProviderInterface[] list of available providers with its configuration
 	 */
 	public $providers = [];
+
+	/**
+	 * @inheritdoc
+	 */
+	public function bootstrap($app)
+	{
+		$modules = $app->getModules();
+		$modules['payment'] = 'cms\payment\frontend\Module';
+		$app->setModules($modules);
+
+		$app->getUrlManager()->addRules([
+			[
+				'pattern' => '/payment/success/<name:[\w\.]+>',
+				'route' => '/payment/success/index',
+			],
+			[
+				'pattern' => '/payment/fail/<name:[\w\.]+>',
+				'route' => '/payment/fail/index',
+			],
+		], false);
+	}
 
 }
